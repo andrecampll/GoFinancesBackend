01a32e052d20835ef3ef31f158e48ddb8f50e9b2
André Victor <andrevictor50@gmail.com>
2020-07-03 19:19:04 -0300

    fixed model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -26,7 +26,7 @@ class Transaction {
   @Column()
   category_id: string;
 
-  @ManyToOne(() => Category, category => category.transaction, { eager: true })
+  @ManyToOne(() => Category, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
